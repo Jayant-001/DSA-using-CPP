10e0607009547c1f8a81093ad1126c982acd3b4a
Pheonix-001 <diplomadear001@gmail.com>
2021-10-21 23:06:14 +0530

    21-10

---
+++ Recursion/permutationOfString.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void permutation(string str, string res) {
+
+    if(str.length() == 0) {
+        cout << res << endl;
+        return;
+    }
+
+    for(int i = 0; i < str.length(); i++){
+        char t = str[i];
+        string ros = str.substr(0, i) + str.substr(i+1);
+
+        permutation(ros, res + t);
+    }
+}
+
+int main() {
+
+    string s;
+    cin >> s;
+    
+    permutation(s, "");
+
+    return 0;
+}
--- practics/recursion/mergeChar.cpp
+++ practics/recursion/mergeChar.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void mergeString(string str1, string str2) {
+
+    for(int i = 0; i < str1.size(); i++){
+        for(int j = 0; j < str2.size(); j++) {
+
+            string res = str1[i] + str2[j];
+            cout << res << endl;
+        }
+    }
+
+}
+
 int main() {
 
+    string str1 = "abc";
+    string str2 = "def";
     
+    mergeString("abc", "str2");
+    return 0;
 }
